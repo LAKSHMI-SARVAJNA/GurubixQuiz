95c2f2572ffcf2abc05b7e2410579458e52eb92f
LAKSHMI-SARVAJNA <sarvajna2004file@gmail.com>
2024-11-21 15:55:57 +0530

    first commit

---
+++ Test-Page/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TestPage from './components/TestPage/TestPage'
+
+function App() {
+  
+
+  return (
+    <div>
+      <TestPage/>
+    </div>
+  )
+}
+
+export default App
