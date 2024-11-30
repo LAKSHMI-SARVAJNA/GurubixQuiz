f0333dd4f485d0f2d2cd849e9d3b54b999243658
LAKSHMI-SARVAJNA <sarvajna2004file@gmail.com>
2024-11-30 14:56:11 +0530

    update

--- src/components/rightCarousel/rightCarousel.jsx
+++ src/components/rightCarousel/rightCarousel.jsx
@@ -8,35 +8,9 @@ import {
   Subcategory,
   Categories
 } from "./styledComponents"; 
-
+import categories from "../../data/Corousel.js"; 
 const RightCarousel = () => {
-  const categories = [
-    {
-      name: "Quantitative Aptitude",
-      subcategories: ["Arithmetic", "Data Interpretation"],
-    },
-    {
-      name: "Verbal (English)",
-      subcategories: ["Verbal Ability", "Verbal Test"],
-    },
-    {
-      name: "Reasoning",
-      subcategories: ["Logical", "Verbal", "Nonverbal"],
-    },
-    {
-      name: "Programming",
-      subcategories: ["Python Programming", "C Programming", "C++, C#", "Java"],
-    },
-    {
-      name: "Interview",
-      subcategories: ["GD", "HR", "Technical Interview"],
-    },
-    {
-      name: "Placement Papers",
-      subcategories: ["Placement Papers", "Submit Paper"],
-    },
-  ];
-
+  
   return (
     <CarouselWrapper>
       <h4>Quick Links</h4>
